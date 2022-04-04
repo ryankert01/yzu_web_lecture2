0e7c32ddcc4d16d6783962addceb5ebd77ad8a3a
ryankert01 <ryan980053@gmail.com>
2022-04-04 11:11:21 +0800

    clubclass add add, search, edit, delete using list.js

--- apps/clubClass/main.js
+++ apps/clubClass/main.js
@@ -1,13 +1,19 @@
 $(function () {
-    $("#courseTable").append("<tr><th>場次</th><th>時間</th><th>主題</th></tr>");
+    $("#courseTable").append(`<thead><tr><th scope="col">#</th><th scope="col">Time</th><th scope="col">Subject</th></tr></thead>`);
     var topicCount = topic.length;
     let millisecsPerDay = 24 * 60 * 60 * 1000;
+    $("#courseTable").append(
+        `<tbody>`
+    );
     for (var x = 0; x < topicCount; x++) {
         //debugger;
         let text1 = (new Date(startDate.getTime()+7*x*millisecsPerDay)).toLocaleDateString();
         const myArray = text1.split("/");
         $("#courseTable").append(
-            `<tr><td>${x+1}</td><td>${myArray[1]}/${myArray[0]}</td><td>${topic[x]}</td></tr>`
+            `<tr><th scope="row">${x+1}</th><td>${myArray[1]}/${myArray[0]}</td><td>${topic[x]}</td></tr>`
         );
     }
+    $("#courseTable").append(
+        `</tbody>`
+    );
 });
